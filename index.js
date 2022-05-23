8f85c2e71f8007a60770a4deb8fc4cfc68eb0118
eliyasmah <eliyasbubt@gmail.com>
2022-05-24 03:02:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4v8pr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const procuctCollection = client.db("car_parts").collection("parts");
+
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = procuctCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello From car parts");
+});
+
+app.listen(port, () => {
+  console.log(`car parts listening on port ${port}`);
+});
