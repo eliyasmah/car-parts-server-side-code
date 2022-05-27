334dffbf719fd7827705af3de2bb7a32a6d6ae5d
eliyasmah <eliyasbubt@gmail.com>
2022-05-28 04:23:53 +0600

    user review fetch

--- index.js
+++ index.js
@@ -41,6 +41,13 @@ async function run() {
       const product = await productCollection.findOne(query);
       res.send(product);
     });
+
+    //POST
+    app.post("/reviews", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productReview.insertOne(newProduct);
+      res.send(result);
+    });
   } finally {
   }
 }
